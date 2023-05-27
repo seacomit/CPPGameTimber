9e0d317bcf7a7039dc5065c7a60bc807c4fba9a6
versatus <86527888+versatus@users.noreply.github.com>
2023-05-27 18:04:29 -0400

    Added the game loop, one texture and sprite for drawing the background, and started drawing!

--- Timber.cpp
+++ Timber.cpp
@@ -10,5 +10,34 @@ int main()
 
 	RenderWindow window(vm, "Timber!", Style::Fullscreen);
 
+	// Create a texture to hold a graphic on the GPU
+	Texture background;
+	background.loadFromFile("graphics/background.png");
+
+	Sprite backgroundSprite;
+	backgroundSprite.setTexture(background);
+	backgroundSprite.setPosition(0, 0);
+
+	while (window.isOpen())
+	{
+		/**
+		 * Handle some input
+		 */
+		if (Keyboard::isKeyPressed(Keyboard::Escape))
+		{
+			window.close();
+		}
+
+		// Update
+
+		// Clear
+		window.clear();
+
+		// Draw
+		window.draw(backgroundSprite);
+
+		window.display();
+	}
+
 	return 0;
 }
